742f0c7780526b58a9bc61143dca706e6b9cb837
Stanislav Matveew <stanislav.matveeew@yandex.ru>
2025-03-25 22:29:05 +0300

    рабочая версия 6

--- src/questionnaire/admin.py
+++ src/questionnaire/admin.py
@@ -19,18 +19,34 @@ def export_responses_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = f'attachment; filename="responses_{questionnaire.title}.csv"'
 
-    writer = csv.writer(response, delimiter='\t')  # Используем табуляцию для разделения колонок
+    writer = csv.writer(response, delimiter='\t')
 
     questions = questionnaire.questions.all()
     question_texts = [q.text for q in questions]
 
-    # Заголовки CSV
-    headers = ['Отметка времени', 'Пол', 'Сколько Вам лет?', 'Рост (в см)', 'Вес (в кг)'] + question_texts
+    # Дополнительные столбцы
+    extra_columns = ['МБФ', 'ФАиРД', 'СТП', 'Образ жизни', 'Питание', 'Пищевое поведение', 'Продукты питания', 'Стресс']
+    headers = (
+        ['Отметка времени', 'Пол', 'Сколько Вам лет?', 'Рост (в см)', 'Вес (в кг)'] +
+        question_texts +
+        extra_columns +
+        ['ИТОГО', 'Оценка уровня соответствия гигиеническим требованиям']
+    )
     writer.writerow(headers)
 
-    # Группируем ответы по session_key
+    # Определяем вопросы по категориям
+    stress_questions = {q.id for q in questions.filter(description="СТРЕСС")}
+    nutrition_questions = {q.id for q in questions.filter(description="ПИТАНИЕ")}
+    eating_behavior_questions = {q.id for q in questions.filter(description="Как часто Вы употребляете следующие продукты и напитки")}
+    work_assessment_questions = {q.id for q in questions.filter(description="САМООЦЕНКА ТРУДОВОГО ПРОЦЕССА")}
+
     responses_by_user = defaultdict(
-        lambda: {'created_at': None, 'gender': '', 'age': '', 'height': '', 'weight': '', 'answers': {}})
+        lambda: {
+            'created_at': None, 'gender': '', 'age': '', 'height': '', 'weight': '',
+            'answers': {}, 'stress_values': [], 'nutrition_values': [],
+            'eating_behavior_values': [], 'work_assessment_values': []
+        }
+    )
 
     responses = UserResponse.objects.filter(questionnaire=questionnaire).select_related('question', 'selected_answer')
 
@@ -49,15 +65,53 @@ def export_responses_csv(modeladmin, request, queryset):
         answer_text = response_obj.selected_answer.text if response_obj.selected_answer else response_obj.free_text_answer
         responses_by_user[session_key]['answers'][response_obj.question.text] = answer_text
 
+        # Если есть числовое значение ответа, добавляем его в соответствующую категорию
+        if response_obj.selected_answer and response_obj.selected_answer.value is not None:
+            if response_obj.question.id in stress_questions:
+                responses_by_user[session_key]['stress_values'].append(response_obj.selected_answer.value)
+            elif response_obj.question.id in nutrition_questions:
+                responses_by_user[session_key]['nutrition_values'].append(response_obj.selected_answer.value)
+            elif response_obj.question.id in eating_behavior_questions:
+                responses_by_user[session_key]['eating_behavior_values'].append(response_obj.selected_answer.value)
+            elif response_obj.question.id in work_assessment_questions:
+                responses_by_user[session_key]['work_assessment_values'].append(response_obj.selected_answer.value)
+
     # Записываем строки с ответами
     for session_key, data in responses_by_user.items():
+        stress_avg = sum(data['stress_values']) / len(data['stress_values']) if data['stress_values'] else ''
+        nutrition_avg = sum(data['nutrition_values']) / len(data['nutrition_values']) if data['nutrition_values'] else ''
+        eating_behavior_avg = sum(data['eating_behavior_values']) / len(data['eating_behavior_values']) if data['eating_behavior_values'] else ''
+        work_assessment_avg = sum(data['work_assessment_values']) / len(data['work_assessment_values']) if data['work_assessment_values'] else ''
+
+        # Собираем значения для столбца ИТОГО (только числа)
+        extra_values = [stress_avg, nutrition_avg, eating_behavior_avg, work_assessment_avg]
+        extra_values = [v for v in extra_values if isinstance(v, (int, float))]  # Убираем пустые значения
+        total_score = sum(extra_values) / len(extra_values) if extra_values else ''
+
+        # Оценка уровня соответствия гигиеническим требованиям
+        if isinstance(total_score, (int, float)):
+            if total_score <= 0.47:
+                hygiene_rating = "Неудовлетворительная"
+            elif 0.47 < total_score <= 0.67:
+                hygiene_rating = "Удовлетворительная"
+            elif 0.67 < total_score <= 0.89:
+                hygiene_rating = "Хорошая"
+            elif 0.9 <= total_score <= 1:
+                hygiene_rating = "Оптимальная"
+            else:
+                hygiene_rating = ""
+        else:
+            hygiene_rating = ""
+
         row = [
-                  data['created_at'],
-                  data['gender'],
-                  data['age'],
-                  data['height'],
-                  data['weight'],
-              ] + [data['answers'].get(q, '') for q in question_texts]
+            data['created_at'],
+            data['gender'],
+            data['age'],
+            data['height'],
+            data['weight'],
+        ] + [data['answers'].get(q, '') for q in question_texts] + [
+            '', '', work_assessment_avg, '', nutrition_avg, eating_behavior_avg, '', stress_avg, total_score, hygiene_rating
+        ]  # Остальные столбцы пока пустые
 
         writer.writerow(row)
 
